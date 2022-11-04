83c3f8ef67fdf1ce2ed2c022554ba3e4448b4d67
Leon7422 <y.kurduban98@gmail.com>
2022-11-04 10:38:33 +0200

    update on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,54 @@
-import { Feedback } from './Feedback/Feedback';
+import React from 'react';
+import { useState } from 'react';
+import { Statistics } from './Feedback/Statistics/Statistics';
+import { FeedbackOptions } from './Feedback/Buttons/Buttons';
+import { Section } from './Feedback/Section/Section';
 
-export const App = () => {
+export default function App() {
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const onLeaveFeedback = options => {
+    setState(prevState => {
+      return { ...prevState, [options]: prevState[options] + 1 };
+    });
+  };
+
+  const countTotalFeedback = () => {
+    return state.good + state.neutral + state.bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const positivePercentage = (
+      (state.good / (state.good + state.neutral + state.bad)) *
+      100
+    ).toFixed(0);
+    if (isNaN(positivePercentage)) {
+      return 0;
+    }
+    return positivePercentage;
+  };
+
   return (
-    <div>
-      <Feedback />
-    </div>
+    <>
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      <Section title={'Statistics'}>
+        <Statistics
+          good={state.good}
+          neutral={state.neutral}
+          bad={state.bad}
+          total={countTotalFeedback}
+          percentage={countPositiveFeedbackPercentage}
+        />
+      </Section>
+    </>
   );
-};
+}
